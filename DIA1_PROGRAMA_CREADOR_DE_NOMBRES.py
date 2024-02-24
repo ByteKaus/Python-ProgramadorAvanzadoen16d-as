c1beb10d2ef39a1a5aa4690a2dda05240557942d
ByteKaus <79665766+ByteKaus@users.noreply.github.com>
2024-02-23 19:51:54 -0500

    Add files via upload

---
+++ DIA1_PROGRAMA_CREADOR_DE_NOMBRES.py
@@ -0,0 +1,4 @@
+print("Hola por favor responde a estas dos preguntas:")
+print("Aqui tienes una sugerencia para el nombre de tu producto: " + "\n" + "'" +
+      input("¿Que es lo que mas te gusta de tu producto? ") + " " + "de" + " " +
+      input("¿Como se llama la ciudad donde naciste? ")+'"')
