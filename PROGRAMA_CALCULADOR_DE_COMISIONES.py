b0203f534954d256ace45a2bc5fd50463d5c5e1c
ByteKaus <79665766+ByteKaus@users.noreply.github.com>
2024-02-24 19:49:17 -0500

    Delete PROGRAMA_CALCULADOR_DE_COMISIONES.py

--- PROGRAMA_CALCULADOR_DE_COMISIONES.py
+++
@@ -1,5 +0,0 @@
-print("Hola, por favor responda las siguientes dos preguntas para calcular la comision que le corresponde.")
-x = input("Ingrese su nombre: ")
-y = float(input("Ingrese las ventas totales realizadas en este mes: "))
-z = round(y*13/100,2)
-print(f"Hola {x}, la comision que le corresponde por las ventas realizadas durante este mes es de: {z} soles")
